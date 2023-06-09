41d6d258b784aa921f6bdba609095cf04ec18862
kimyoungjae0412 <kings@us-all.co.kr>
2023-06-09 18:36:57 +0900

    0609

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board-status.enum';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions: string[] = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is not in the status options`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: string) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
